9900b48b057c3dc5554ec13f327c7ff629ecd33e
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-07 21:47:40 +0100

    Movement to left and right

---
+++ Bobm Dodger/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 currentPosition = transform.position; //x,y Axis
+
+        if (h>0)
+        {
+            //going to the right side
+            currentPosition.x += speed * Time.deltaTime;
+            
+        }
+        else if(h<0)
+        {
+            //going to the left side
+            currentPosition.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = currentPosition;
+    }
+
+} // end PlayerScript
